40bd5390112cf3e90bf817a5dc6f2089b6f0bfad
Marin Garcia <d_maringarcia@ats-hr420-dm16.fc.ca>
2020-01-21 08:59:41 -0500

    setting movies filters

---
+++ templates/header.php
@@ -0,0 +1,11 @@
+<header>
+    <h2>This is the header</h2>
+    <nav class="filterNav">
+    <ul>
+        <li><a href="index.php?filter=action">Action</a></li>
+        <li><a href="index.php?filter=comedy">comedy</a></li>
+        <li><a href="index.php?filter=family">family</a></li>
+        <li><a href="index.php?">All</a></li>
+    </ul>
+    </nav>
+</header>
